f3159865f97c8ee2fd4d17ddb4d2a4cd14e484ca
Andrew James Kinsey <akinsey@Andrews-MacBook-Air-123.local>
2017-12-05 12:36:52 -0600

    done with first api request to get movie id

--- Movie Time/ViewController.swift
+++ Movie Time/ViewController.swift
@@ -9,17 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var myWebView: UIWebView!
+    @IBOutlet weak var searchTextField: UITextField!
+    let apiKey = "ab49369946fca622201b54753bf05125"
+    var movieID = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getVideo(videoCode: "pPEfojnodKY")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getVideo(videoCode: String)
+    {
+        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+        myWebView.loadRequest(URLRequest(url: url!))
     }
-
+    
+    
+    @IBAction func searchButtonTapped(_ sender: Any)
+    {
+        if searchTextField.text == nil || searchTextField.text == ""
+        {
+        }
+        else
+        {
+            getMovieID()
+            let query = "https://api.themoviedb.org/3/movie/\(movieID)?api_key=\(apiKey)&append_to_response=videos"
+            DispatchQueue.global(qos: .userInitiated).async
+                {
+                    [unowned self] in
+                    if let url = URL(string: query)
+                    {
+                        if let data = try?Data(contentsOf: url)
+                        {
+                            let json = try?JSON(data: data)
+                            
+                        }
+                        else
+                        {
+                            //solve error
+                        }
+                    }
+            }
+        }
+        
+    }
+    
+    func getMovieID()
+    {
+        let searchedMovie = searchTextField.text
+        let searchQuery = "https://api.themoviedb.org/3/search/movie?api_key=\(apiKey)&query=\(searchedMovie ?? apiKey)"
+        DispatchQueue.global(qos: .userInitiated).async
+            {
+                [unowned self] in
+                if let url = URL(string: searchQuery)
+                {
+                    if let data = try?Data(contentsOf: url)
+                    {
+                        let json = try?JSON(data: data)
+                        self.parseQuery(json: json!)
+                        return
+                    }
+                    else
+                    {
+                        //solve error
+                    }
+                }
+        }
+    }
+    
+    func parse(json: JSON)
+    {
+        
+    }
+    
+    func parseQuery(json: JSON)
+    {
+        for result in json["results"].arrayValue
+        {
+            let id = result["id"].stringValue
+            movieID = id
+            return
+        }
+    }
+    
 
 }
 
