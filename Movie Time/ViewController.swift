813f9421a7ae17f143fd0ee01d301c917d16ed14
Andrew Kinsey <akinsey@andrews-macbook-air-123.local>
2017-12-18 12:10:40 -0600

    done with coding aspects, about to work on ui

--- Movie Time/ViewController.swift
+++ Movie Time/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     let apiKey = "ab49369946fca622201b54753bf05125"
     var movieID = String()
     var videoKey = String()
+    var canMoveOn = Bool()
+    var jsonResults = String()
     
     override func viewDidLoad()
     {
@@ -32,23 +34,37 @@ class ViewController: UIViewController {
     {
         if searchTextField.text == nil || searchTextField.text == ""
         {
+            let alert = UIAlertController(title: "No text was entered in the field.", message: "", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
         else
         {
             getMovieID()
-            getVideoCode()
-            getVideo(videoCode: videoKey)
+            
+            let deadline = DispatchTime.now() + 1
+            DispatchQueue.main.asyncAfter(deadline: deadline)
+            {
+                if self.canMoveOn == true
+                {
+                self.getVideoCode()
+
+                let deadline = DispatchTime.now() + 1
+                DispatchQueue.main.asyncAfter(deadline: deadline)
+                    {
+                        self.getVideo(videoCode: self.videoKey)
+                    }
+                }
+            }
         }
-        
     }
     
     func getMovieID()
     {
-        let searchedMovie = searchTextField.text
-        let searchQuery = "https://api.themoviedb.org/3/search/movie?api_key=\(apiKey)&query=\(searchedMovie ?? apiKey)"
-        DispatchQueue.global(qos: .userInitiated).async
-            {
-                [unowned self] in
+        let textFieldText = searchTextField.text
+        let searchedMovie = textFieldText?.replacingOccurrences(of: " ", with: "%20")
+        let searchQuery = "https://api.themoviedb.org/3/search/movie?api_key=\(apiKey)&query=\(searchedMovie ?? "Home%20Alone")"
+
                 if let url = URL(string: searchQuery)
                 {
                     if let data = try?Data(contentsOf: url)
@@ -59,14 +75,19 @@ class ViewController: UIViewController {
                     }
                     else
                     {
-                        //solve error
+                        self.canMoveOn = false
+                        let alert = UIAlertController(title: "An ERROR has been encountered.", message: "Try again later.", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
                     }
                 }
-        }
+        
     }
     
     func getVideoCode()
     {
+        if canMoveOn == true
+        {
         let query = "https://api.themoviedb.org/3/movie/\(movieID)?api_key=\(apiKey)&append_to_response=videos"
         DispatchQueue.global(qos: .userInitiated).async
             {
@@ -82,9 +103,13 @@ class ViewController: UIViewController {
                     }
                     else
                     {
-                        //solve error
+                        self.canMoveOn = false
+                        let alert = UIAlertController(title: "An ERROR has been encountered.", message: "Try again later.", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
                     }
                 }
+            }
         }
     }
     
@@ -101,13 +126,30 @@ class ViewController: UIViewController {
     
     func parseQuery(json: JSON)
     {
-        for result in json["results"].arrayValue
+        jsonResults = "\(json["total_results"])"
+        print(jsonResults)
+        canMoveOn = false
+        if jsonResults == "0"
         {
-            let id = result["id"].stringValue
-            movieID = id
-            return
+            canMoveOn = false
+            let alert = UIAlertController(title: "No movie was found with that title.", message: "Please check the spelling and try again later.", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else
+        {
+            canMoveOn = true
+            for result in json["results"].arrayValue
+            {
+                let id = result["id"].stringValue
+                movieID = id
+                return
+            }
         }
     }
+   
+    
+    
     
 
 }
